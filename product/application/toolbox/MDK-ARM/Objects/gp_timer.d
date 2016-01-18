e510bd97f2514e6bb363b7f2f2851cc2eef9cf0d
qian.yao <qian.yao@jumacc.com>
2016-01-18 18:52:41 +0800

    Remove useless files.

--- product/application/toolbox/MDK-ARM/Objects/gp_timer.d
+++
@@ -1,7 +0,0 @@
-.\objects\gp_timer.o: ..\..\..\..\System\DRIVERS\BlueNRG\Src\gp_timer.c
-.\objects\gp_timer.o: ..\..\..\..\System\JUMA\Inc\clock.h
-.\objects\gp_timer.o: ..\..\..\..\System\DRIVERS\BlueNRG\Inc\hal_types.h
-.\objects\gp_timer.o: C:\nigel\software\keil_mdk514\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\gp_timer.o: ..\..\..\..\System\DRIVERS\BlueNRG\Inc\gp_timer.h
-.\objects\gp_timer.o: ..\..\..\..\System\DRIVERS\BlueNRG\Inc\clock.h
-.\objects\gp_timer.o: ..\..\..\..\System\DRIVERS\BlueNRG\Inc\ble_status.h
--- product/application/toolbox/MDK-ARM/Objects/x_nucleo_iks01a1_pressure.d
+++
@@ -1,6 +0,0 @@
-.\objects\x_nucleo_iks01a1_pressure.o: ..\..\..\..\System\BSP\NUCLEO\x_nucleo_iks01a1_pressure.c
-.\objects\x_nucleo_iks01a1_pressure.o: ..\..\..\..\System\BSP\NUCLEO\x_nucleo_iks01a1_pressure.h
-.\objects\x_nucleo_iks01a1_pressure.o: ..\..\..\..\System\DRIVERS\LPS25H\lps25h.h
-.\objects\x_nucleo_iks01a1_pressure.o: ..\..\..\..\System\DRIVERS\COMMON\pressure.h
-.\objects\x_nucleo_iks01a1_pressure.o: C:\nigel\software\keil_mdk514\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\x_nucleo_iks01a1_pressure.o: ..\..\..\..\System\DRIVERS\LPS25HB\lps25hb.h
